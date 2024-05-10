a9b0a2f281daf7c285d84de4c0b7140368654f77
Tamakuz <jovangithub@gmail.com>
2024-05-11 02:15:49 +0700

    fix image

--- src/components/reuseble/nav-reuseble.tsx
+++ src/components/reuseble/nav-reuseble.tsx
@@ -26,8 +26,8 @@ const NavDesktop = () => {
         <Image
           src={BrandImage}
           alt="brand"
-          width={"auto"}
-          height={"auto"}
+          width={100}
+          height={100}
           priority
         />
       </div>
