b1e4c4ff35d06388e63748a7300bed9150dd927b
Tamakuz <jovangithub@gmail.com>
2024-08-07 02:34:08 +0700

    ADD: stream

--- src/components/common/movie-common.tsx
+++ src/components/common/movie-common.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import HeroMovieAtomic from "../atomic/hero-movie-atomic";
-import CastCrewMovieAtomic from "../atomic/cast-crew-movie-atomic";
-import MediaMovieAtomic from "../atomic/media-movie-atomic";
-import RecomendationMovieAtomic from "../atomic/rcomrndation-movie-atomic";
 
 const MovieCommon = ({ idMovie }: { idMovie: string }) => {
   return (
